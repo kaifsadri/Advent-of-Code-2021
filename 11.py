526700bde654119d5436e6a3dfc779aa464393c6
Kai F. Sadri <58483176+kaifsadri@users.noreply.github.com>
2022-04-03 11:47:26 -0400

    more efficient code

--- 11.py
+++ 11.py
@@ -1,42 +1,34 @@
 L = [line.strip() for line in open("11.txt").readlines()]
-
-
-def Step():
-    global G
-    for dumbo in G:
-        G[dumbo] += 1
-    F = set(d for d in G if G[d] > 9)
-    A = F.copy()
-    while F:
-        dumbo = F.pop()
-        G[dumbo] = 0
-        for coord in {(0, 1), (-1, -1), (-1, 1), (1, 1), (1, -1), (-1, 0), (1, 0), (0, -1)}:
-            c = (dumbo[0] + coord[0], dumbo[1] + coord[1])
-            if c in G and c not in A:
-                G[c] += 1
-                if G[c] > 9:
-                    F.add(c)
-                    A.add(c)
-    return len(A)
-
-
-# part 1: 100 steps
 G = dict()
 for row, line in enumerate(L):
     for col, dumbo in enumerate(line):
         G[(row, col)] = int(dumbo)
-N = 0
-for step in range(100):
-    N += Step()
-print(f"Part 1: {N}")
-
-# part 2: Sync means Step() returns 100
-for row, line in enumerate(L):
-    for col, dumbo in enumerate(line):
-        G[(row, col)] = int(dumbo)
-Steps = 0
+steps = 0
+tot_fl = 0
 while True:
-    Steps += 1
-    if 100 == Step():
+    F = set()
+    A = set()
+    for dumbo in G:
+        G[dumbo] += 1
+        if G[dumbo] > 9:
+            F.add(dumbo)
+    while F:
+        dumbo = F.pop()
+        G[dumbo] = 0
+        A.add(dumbo)
+        for r in range(-1, 2):
+            for c in range(-1, 2):
+                d = (dumbo[0] + r, dumbo[1] + c)
+                if d in G and d not in A:
+                    G[d] += 1
+                    if G[d] > 9:
+                        F.add(d)
+    steps += 1
+    tot_fl += len(A)
+    # part 1: 100 steps
+    if steps == 100:
+        print(f"Part 1: {tot_fl}")
+    # part 2: Sync means we had 100 flashes
+    if len(A) == 100:
+        print(f"Part 2: {steps}")
         break
-print(f"Part 2: {Steps}")
